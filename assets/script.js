3d143e04aed8c3950db56a031ab1ce7c6617fab7
Josef K <ahmed.abshir91@gmail.com>
2023-09-24 01:06:59 +0100

    added javascript to project

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var topic = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+// if (topic === 'HTML') {
+//   console.log("Let's study HTML!");
+// } else if (topic === 'CSS') {
+//   console.log("Let's study CSS!");
+// } else if (topic === 'Git') {
+//   console.log("Let's study Git!");
+// } else if (topic === 'JavaScript') {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log('Please try again!');
+// }
+
+for(var i = 0; i < topic.length; i++ ){
+    console.log(topic[i]);
+}
